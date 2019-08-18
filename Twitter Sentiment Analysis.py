1d70fc16dfa680ed7a82a28555c04f9048febc64
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-18 21:46:55 +0200

    logged in twitter API

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -11,8 +11,8 @@ from textblob import TextBlob
 # Using Twitter API created a twitter app and generated access tokens
 from tweepy.auth import AuthHandler
 
-consumer_key = "xymCHFLBMXmVstjskVbQV9ubP"
-consumer_secret = "5ULTY4FyECVc7w4xMXRlH2kYej9erJw9e2rYxmzklRJ0rRb6Ci"
+consumer_key = "xymCHFLBMXmvStjskVbQV9ubP"
+consumer_secret = "5ULTY4FyECVc7w4xMXRlH2KUej9erJw9e2rYxmzklRJ0rRb6Ci"
 
 access_token = "2328844606-L8LSWoVRuKa4LATCVcL6YgeWso1rr2gB6duDmZx"
 access_token_sectet = "b061CB0XDN3vEl7psSJAmrSZuMWLifwDbi8JLODWWnlNq "
