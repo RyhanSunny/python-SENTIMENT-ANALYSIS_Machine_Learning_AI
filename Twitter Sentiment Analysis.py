2b060e5068091eae87cc4f5bc823c02f586571cc
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-18 21:37:01 +0200

    logged in twitter API

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -17,5 +17,9 @@ consumer_secret = "5ULTY4FyECVc7w4xMXRlH2kYej9erJw9e2rYxmzklRJ0rRb6Ci"
 access_token = "2328844606-L8LSWoVRuKa4LATCVcL6YgeWso1rr2gB6duDmZx"
 access_token_sectet = "b061CB0XDN3vEl7psSJAmrSZuMWLifwDbi8JLODWWnlNq "
 
+# logging in (O auth not zero)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_sectet)
 
+api = tweepy.API(auth)
 
