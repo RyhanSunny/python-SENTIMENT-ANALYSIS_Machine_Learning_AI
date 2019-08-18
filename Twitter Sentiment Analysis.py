3804cf42feb3cddfd57d933e40c4ca08b13c0d02
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-18 21:36:34 +0200

    accessed twitter API

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -1,9 +1,21 @@
 import tweepy
+import tweepy as tweepy
 from textblob import TextBlob
 
-wiki = TextBlob('Siraj is angry he never get avacados for Cristmas')
-wiki.tags
-wiki.words
-wiki.sentiment.polarity
-## waiting for twitter to accept my developers account request
+# wiki = TextBlob('Sunny is angry he never get avocados for Christmas')
+# wiki.tags
+# wiki.words
+# wiki.sentiment.polarity
+
+
+# Using Twitter API created a twitter app and generated access tokens
+from tweepy.auth import AuthHandler
+
+consumer_key = "xymCHFLBMXmVstjskVbQV9ubP"
+consumer_secret = "5ULTY4FyECVc7w4xMXRlH2kYej9erJw9e2rYxmzklRJ0rRb6Ci"
+
+access_token = "2328844606-L8LSWoVRuKa4LATCVcL6YgeWso1rr2gB6duDmZx"
+access_token_sectet = "b061CB0XDN3vEl7psSJAmrSZuMWLifwDbi8JLODWWnlNq "
+
+
 
