bc0ee1d8e2d80dccbdde86d674184d7355b59d1f
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-19 22:37:50 +0200

    search for a keyword and print all the tweets that contain the keyword

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -23,3 +23,14 @@ auth.set_access_token(access_token, access_token_sectet)
 
 api = tweepy.API(auth)
 
+# now that we have our API set up we can perform a whole bunch of possible methods
+# such as: create tweet, delete tweet, find twitter user
+
+# for our use case we will collect tweets using a certain keyword
+
+# search keyword:
+public_tweets = api.search('Trump')
+# print all out in the terminal
+for tweet in public_tweets:
+    print(tweet.text)
+
